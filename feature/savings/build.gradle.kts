33a5b61fbf5ceecbc0a541bf71ce2e17076f08c0
Pronay sarker <pronaycoding@gmail.com>
2024-08-02 09:48:48 +0600

    move savings to savings module

---
+++ feature/savings/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.mifos.android.feature)
+    alias(libs.plugins.mifos.android.library.compose)
+    alias(libs.plugins.mifos.android.library.jacoco)
+}
+
+android {
+    namespace = "com.mifos.feature.savings"
+}
+
+dependencies {
+    implementation(projects.core.domain)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
